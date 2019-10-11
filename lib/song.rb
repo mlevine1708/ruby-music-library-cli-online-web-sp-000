579b07e7bd0ac17a728e3d4d7a40730f4887b6b7
Micah Levine <micah.levine@outlook.com>
2019-10-11 01:54:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,74 +1,65 @@
 class Song
+  attr_accessor :name
+  attr_reader :artist, :genre
 
-attr_accessor :name, :artist, :genre
-extend Concerns::Findable
+  @@all = []
 
-@@all = []
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
 
-def initialize(name, artist=nil, genre=nil)
-  @name = name
-  self.artist=(artist) if artist !=nil
-  self.genre=(genre) if genre != nil
-end
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
 
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
 
-def self.all
-  @@all
-end
+  def self.all
+    @@all
+  end
 
-def self.find_by_name(name)
-  all.detect{ |s| s.name == name }
-end
+  def self.destroy_all
+    all.clear
+  end
 
-def self.find_or_create_by_name(name)
-  find_by_name(name) || create(name)
-end
+  def save
+    self.class.all << self
+  end
 
-def save
-  @@all << self
-end
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
 
-def self.create(song)
-  song = self.new(name)
-  song.save
-  song
-end
+    # Or, as a one-liner:
+    # new(name).tap{ |s| s.save }
+  end
 
-def self.destroy_all
-  @@all.clear
-end
+  def self.find_by_name(name)
+    all.detect{ |s| s.name == name }
+  end
 
-def artist
-  @artist
-end
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
 
-def genre
-  @genre
-end
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
 
-def artist=(artist)
-  @artist = artist
-  artist.add_song(self)
-end
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
 
-def genre=(genre)
-  @genre = genre
-  genre.songs << self unless genre.songs.include?(self)
-end
+    new(song_name, artist, genre)
+  end
 
-def self.new_from_filename(filename)
-  array = filename.split(" - ")
-
-  song_name = array[1]
-  artist_name = array[0]
-  genre_name = array[2].split(".mp3").join
-
-  artist = Artist.find_or_create_by_name(artist_name)
-  genre = Genre.find_or_create_by_name(genre_name)
-  self.new(song_name, artist, genre)
-end
-
-def self.create_from_filename(filename)
-  self.new_from_filename(filename).save
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{ |s| s.save }
   end
 end
